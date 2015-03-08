2729391d617b8405e185d635b859e969759360ea
Alexis Lescouet <alexis.lescouet@telecom-sudparis.eu>
2015-03-08 14:31:13 +0100

    First version of test files for graph.[hc] objects
    	new file:   tests/unit/include/graph_suite.h
    	new file:   tests/unit/src/cunit_graph.c
    	new file:   tests/unit/src/graph_suite.c

---
+++ tests/unit/include/graph_suite.h
@@ -0,0 +1,10 @@
+/* Copyright 2015 <Copyright Owner> */
+
+#include "CUnit/Basic.h"
+#include "../../../chessaint/include/graph.h"
+
+int init_suite_graph(void);
+
+int clean_suite_graph(void);
+
+int test_create_arc(void);
---
+++ tests/unit/src/graph_suite.c
@@ -0,0 +1,29 @@
+/* Copyright 2015 <Copyright Owner> */
+
+#include "CUnit/Basic/h"
+#include "../../../chessaint/include/graph.h"
+
+/* The suite initialization function.
+ * Returns zero on success, non-zero otherwise.
+ */
+int init_suite_graph(void) {
+/* 
+ *   if (problem during initialisation)
+ *     return -1; // this number can be used to explicit the problem
+ */
+  return 0;
+}
+
+/* The suite cleanup function.
+ * Returns zero on success, non-zero otherwise.
+ */
+int clean_suite_graph(void) {
+/* 
+ *   if (problem during cleaning)
+ *     return -1; // this number can be used to explicit the problem
+ */
+  return 0;
+}
+
+void test_create_arc(void) {
+

68d288080bf47ec8c8bf258c9aa492588ad3ccea
Alexis Lescouet <alexis.lescouet@telecom-sudparis.eu>
2015-03-30 01:07:30 +0200

    Patch documentation of members Arc, Element, Llist (typedefs) 	modified:   chessaint/include/graph.h

--- chessaint/include/graph.h
+++ chessaint/include/graph.h
@@ -17,9 +17,10 @@
 /** 
  *  @enum Set
  *  @brief Which set belongs the item
- *  
- *  This enumeration represents the set in which is the item for astar calculation
- *  
+ *
+ *  This enumeration represents the set in which
+ *  is the item for astar calculation
+ *
  */
 
 enum Set {
@@ -30,15 +31,24 @@ enum Set {
 
 
 /** 
+ *  @typedef Arc
+ *  @brief Definition of the Arc type
+ *
+ *  Defines struct Arc as the Arc type
+ *
+ */
+
+typedef struct Arc Arc;
+
+/** 
  *  @struct Arc
  *  @brief Represents the arc between two nodes of a graph
  *  @bug Problem of initialisation
  *
  *  It stores the game data we need, according to the data contained
- * in a FEN string (5 last attributes).
+ *  in a FEN string (5 last attributes).
  */
 
-typedef struct Arc Arc;
 struct Arc {
   int id;                /**< The id of the arc */
   char from[3];          /**< from  */
@@ -79,16 +89,33 @@ struct Arc {
 };
 
 /** 
+ *  @typedef Element
+ *  @brief Definition of the Element type
+ *
+ *  Defines struct Element as the Element type
+ *
+ */
+
+typedef struct Element Element;
+
+/** 
  *  @struct Element
  *  @brief Definition of an list element
  *
  */
 
-typedef struct Element Element;
 struct Element {
   Arc value; /**< The value of the element, an arc */
   struct Element *next; /**< The link to the next element of the list */
 };
+
+/** 
+ *  @typedef Llist
+ *  @brief Definition of the Llist type
+ *
+ *  Defines type Llist as a pointer on type Element (=struct Element).
+ *
+ */
 
 typedef Element *Llist;
 
@@ -96,10 +123,8 @@ void llist_add(Arc newvalue, Llist *list);
 int llist_suppr(Llist *list);
 void llist_free(Llist *list);
 
-void arc_init(Arc *arc);
 void arc_print(Arc value);
 int arc_equal(Arc left, Arc right);
 void llist_print(Llist list);
 
 #endif /*TRUNK_CHESSAINT_INCLUDE_GRAPH_H_*/
-

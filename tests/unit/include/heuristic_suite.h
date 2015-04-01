6eb0468e78818e6e6d486ceb3a3ee23aa130fa5a
Alexis Lescouet <alexis.lescouet@telecom-sudparis.eu>
2015-04-01 16:48:32 +0200

    Correct an small error 	modified:   tests/unit/include/heuristic_suite.h

--- tests/unit/include/heuristic_suite.h
+++ tests/unit/include/heuristic_suite.h
@@ -1,10 +1,10 @@
 /* This file is part of the ChessAInt project 2015 */
 
-#ifndef TESTS_UNIT_INCLUDE_GRAPH_SUITE_H_
-#define TESTS_UNIT_INCLUDE_GRAPH_SUITE_H_
+#ifndef TESTS_UNIT_INCLUDE_HEURISTIC_SUITE_H_
+#define TESTS_UNIT_INCLUDE_HEURISTIC_SUITE_H_
 
 #include "CUnit/Basic.h"
-#include "include/graph.h"
+#include "include/heuristic.h"
 
 int init_suite_heuristic(void);
 int clean_suite_heuristic(void);
@@ -13,4 +13,4 @@ void test_is_mat(void);
 void test_number_of_char(void);
 void test_heuristic_fen(void);
 
-#endif /* TESTS_UNIT_INCLUDE_GRAPH_SUITE_H_ */
+#endif /* TESTS_UNIT_INCLUDE_HEURISTIC_SUITE_H_ */
